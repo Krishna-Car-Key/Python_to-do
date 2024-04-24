c7e7b3b9f7a6a9d7d175465478b4222564b116e0
krishna <krishnakarki1996@gmail.com>
2024-04-24 19:09:35 +0545

    implement complete button

--- python-todo/gui.py
+++ python-todo/gui.py
@@ -3,21 +3,29 @@ from modules import functions
 
 user_input_title = sg.Text("To-Do Box ")
 user_input_box = sg.Input(tooltip="Enter todo", key='todo')
-add_box = sg.Button("add")
+add_box = sg.Button("Add")
+
+# in Listbox we can use the value to store the list-
+# -like Listbox(value=functions.return_todos())
+# however i am not applying this because my function is running normally
+
 list_box = sg.Listbox(functions.return_todos(),
                       enable_events=True,
                       size=(45, 10), key='todos')
-edit_button = sg.Button("edit")
+edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+
+layout = [[user_input_title], [user_input_box, add_box], [list_box, edit_button,complete_button]]
 
 window = sg.Window('My To-Do Lists',
-                   layout=[[user_input_title], [user_input_box, add_box], [list_box, edit_button]],
+                   layout=layout,
                    font=('Helvetica', 16))
 
 while True:
     event, value = window.read()
     print(event, value)
     match event:
-        case 'add':
+        case 'Add':
             todos = functions.return_todos()
 
             new_todo = value['todo'].title() + '\n'
@@ -25,13 +33,21 @@ while True:
 
             functions.write_todos(todos)
             window['todos'].update(todos)
-        case 'edit':
+        case 'Edit':
             print(value['todos'])
             todos = functions.return_todos()
             index = todos.index(value['todos'][0])
             todos[index] = value['todo'] + '\n'
             functions.write_todos(todos)
             window['todos'].update(todos)
+        case 'Complete':
+            todo_to_complete = value['todos'][0]
+            todos = functions.return_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(todos)
+            window['todo'].update('')
+
         case 'todos':
             value['todo'] = value['todos'][0]
             print(value['todo'])
